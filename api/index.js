8c5ff4d48a2b065c3ae98216865fab0246df9121
Elvis Fagundes <elvis.f.lisboa.jr@hotmail.com>
2022-05-15 22:06:18 -0300

    NodeJS API REST

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
+const roteador = require('./rotas/fornecedores')
+
+app.use('/api/fornecedores', roteador)
+
+app.use(bodyParser.json())
+
+app.listen(config.get('api.porta'), () => console.log('API ok'))    
